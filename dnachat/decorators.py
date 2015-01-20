49cea98350bbff9ac62a6d2300db29945c56dd79
acuros <acuros@kaist.ac.kr>
2015-01-21 08:36:03 +0900

    auth_required added

--- dnachat/decorators.py
+++ dnachat/decorators.py
@@ -4,6 +4,15 @@ from functools import wraps
 from dnachat.dna.exceptions import ProtocolError
 
 
+def auth_required(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if self.user is None:
+            raise ProtocolError
+        return func(self, *args, **kwargs)
+    return wrapper
+
+
 def in_channel_required(func):
     @wraps(func)
     def wrapper(self, *args, **kwargs):
