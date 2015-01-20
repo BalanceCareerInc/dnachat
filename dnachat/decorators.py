e51a36ddc3131b2bc6e20606d3e4dfdfba24553a
acuros <acuros@kaist.ac.kr>
2015-01-20 21:40:56 +0900

    must_be_in_channel bug fixed

--- dnachat/decorators.py
+++ dnachat/decorators.py
@@ -7,7 +7,7 @@ from dnachat.dna.exceptions import ProtocolError
 def must_be_in_channel(func):
     @wraps(func)
     def wrapper(self, *args, **kwargs):
-        if self.user and self.user.channel is None:
+        if self.user is not None and self.user.channel is None:
             raise ProtocolError
         return func(self, *args, **kwargs)
     return wrapper
