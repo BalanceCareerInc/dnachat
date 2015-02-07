976eba25fd44996b6251e17a90afa9b02a32067a
acuros <acuros@kaist.ac.kr>
2015-02-07 19:56:32 +0900

    Allow multiple request in one message

--- dnachat/dna/protocol.py
+++ dnachat/dna/protocol.py
@@ -1,4 +1,5 @@
 # -*-coding:utf8-*-
+import struct
 from twisted.internet.protocol import Protocol
 
 from .request import Request
@@ -7,11 +8,20 @@ from ..logger import logger
 
 
 class DnaProtocol(Protocol):
-    def dataReceived(self, raw_bson):
-        try:
-            request = Request.from_bson(raw_bson)
-            logger.info('"%s" Received' % request.method)
-            self.requestReceived(request)
-        except ProtocolError, e:
-            logger.error('ProtocolError: %s' % str(e), exc_info=True)
-            self.transport.loseConnection()
+    def dataReceived(self, packet):
+        while True:
+            try:
+                size = struct.unpack('<i', packet[:4])[0]
+            except struct.error:
+                self.transport.loseConnection()
+            raw_bson = packet[:size]
+            packet = packet[size:]
+            try:
+                request = Request.from_bson(raw_bson)
+                logger.info('"%s" Received' % request.method)
+                self.requestReceived(request)
+            except ProtocolError, e:
+                logger.error('ProtocolError: %s' % str(e), exc_info=True)
+                self.transport.loseConnection()
+            if packet == '':
+                break
