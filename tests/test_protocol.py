25452bb71b4a53fa4a11603d67843a596b8243b1
Acuros <acuros@kaist.ac.kr>
2015-03-16 14:44:52 +0900

    Test file refactored

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -10,6 +10,20 @@ import config
 bson.patch_socket()
 
 
+class AuthenticatedClient(object):
+    def __init__(self, user):
+        self.conn = socket(AF_INET, SOCK_STREAM)
+        self.conn.connect(('localhost', config.PORT))
+        self.conn.sendobj(dict(method='authenticate', id=user))
+        assert self.conn.recvobj() == dict(method=u'authenticate', status=u'OK')
+
+    def __enter__(self):
+        return self.conn
+
+    def __exit__(self, type, value, traceback):
+        self.conn.close()
+
+
 @fixture
 def user1():
     return 'id1'
@@ -22,7 +36,6 @@ def user2():
 
 @fixture
 def channel1(user1, user2):
-    print 1
     channels = Channel.create_channel([user1, user2])
     return channels[0].name
 
@@ -33,40 +46,24 @@ def group_chat_channel1(user1):
     return channel.name
 
 
-def test_authenticate():
-    client_sock = socket(AF_INET, SOCK_STREAM)
-    client_sock.connect(('localhost', config.PORT))
-    client_sock.sendobj(dict(method='authenticate', id='id'))
-    response = client_sock.recvobj()
-    client_sock.close()
-    assert response['method'] == u'authenticate'
-    assert response['status'] == u'OK'
+def test_authenticate(user1):
+    with AuthenticatedClient(user1) as client_sock:
+        pass
 
 
-def test_create():
-    client_sock = socket(AF_INET, SOCK_STREAM)
-    client_sock.connect(('localhost', config.PORT))
-    client_sock.sendobj(dict(method='authenticate', id='id1'))
-    client_sock.recvobj()
-
-    client_sock.sendobj(dict(method='create', partner_id=u'id2'))
-    response = client_sock.recvobj()
-    client_sock.close()
-
+def test_create(user1):
+    with AuthenticatedClient(user1) as client_sock:
+        client_sock.sendobj(dict(method='create', partner_id=u'id2'))
+        response = client_sock.recvobj()
     assert response['method'] == 'create'
     assert 'channel' in response
     assert response['partner_id'] == 'id2'
 
 
-def test_join(group_chat_channel1, user1, user2):
-    client_sock = socket(AF_INET, SOCK_STREAM)
-    client_sock.connect(('localhost', config.PORT))
-    client_sock.sendobj(dict(method='authenticate', id=user2))
-    client_sock.recvobj()
-
-    client_sock.sendobj(dict(method='join', channel=group_chat_channel1))
-    response = client_sock.recvobj()
-    client_sock.close()
+def test_join(group_chat_channel1, user1):
+    with AuthenticatedClient(user1) as client_sock:
+        client_sock.sendobj(dict(method='join', channel=group_chat_channel1))
+        response = client_sock.recvobj()
 
     assert response['method'] == 'join'
     assert response['channel'] == group_chat_channel1
@@ -74,14 +71,9 @@ def test_join(group_chat_channel1, user1, user2):
 
 
 def test_withdrawal(group_chat_channel1, user1):
-    client_sock = socket(AF_INET, SOCK_STREAM)
-    client_sock.connect(('localhost', config.PORT))
-    client_sock.sendobj(dict(method='authenticate', id=user1))
-    client_sock.recvobj()
-
-    client_sock.sendobj(dict(method='withdrawal', channel=group_chat_channel1))
-    response = client_sock.recvobj()
-    client_sock.close()
+    with AuthenticatedClient(user1) as client_sock:
+        client_sock.sendobj(dict(method='withdrawal', channel=group_chat_channel1))
+        response = client_sock.recvobj()
 
     assert response['method'] == 'withdrawal'
     try:
@@ -93,43 +85,30 @@ def test_withdrawal(group_chat_channel1, user1):
 
 
 def test_attend(channel1, user1):
-    client_sock = socket(AF_INET, SOCK_STREAM)
-    client_sock.connect(('localhost', config.PORT))
-    client_sock.sendobj(dict(method='authenticate', id=user1))
-    client_sock.recvobj()
+    with AuthenticatedClient(user1) as client_sock:
+        client_sock.sendobj(dict(method='attend', channel=channel1))
+        response = client_sock.recvobj()
 
-    client_sock.sendobj(dict(method='attend', channel=channel1))
-    response = client_sock.recvobj()
     assert response['method'] == 'attend'
     assert response['channel'] == channel1
     assert time.time() - response['last_read'] < 1.0
-    client_sock.close()
 
 
 def test_publish(channel1, user1, user2):
-    sock1 = socket(AF_INET, SOCK_STREAM)
-    sock1.connect(('localhost', config.PORT))
-    sock1.sendobj(dict(method='authenticate', id=user1))
-    sock1.recvobj()
-    sock1.sendobj(dict(method='attend', channel=channel1))
-    sock1.recvobj()
+    with AuthenticatedClient(user1) as sock1:
+        sock1.sendobj(dict(method='attend', channel=channel1))
+        sock1.recvobj()
 
-    sock2 = socket(AF_INET, SOCK_STREAM)
-    sock2.connect(('localhost', config.PORT))
-    sock2.sendobj(dict(method='authenticate', id=user2))
-    sock2.recvobj()
-    sock2.sendobj(dict(method='attend', channel=channel1))
-    sock2.recvobj()
+        with AuthenticatedClient(user2) as sock2:
+            sock2.sendobj(dict(method='attend', channel=channel1))
+            sock2.recvobj()
 
-    sock1.sendobj(dict(method='publish', message='Hi!', type='text'))
-    response1 = sock1.recvobj()
-    response2 = sock2.recvobj()
+            sock1.sendobj(dict(method='publish', message='Hi!', type='text'))
+            response1 = sock1.recvobj()
+            response2 = sock2.recvobj()
 
     assert response1 == response2
     assert response1['method'] == 'publish'
     assert response1['writer'] == user1
     assert response1['message'] == 'Hi!'
-
-    sock1.close()
-    sock2.close()
 
