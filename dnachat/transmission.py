e6aa731efd994a52846adf78bae72235e86956f0
acuros <acuros@kaist.ac.kr>
2015-07-14 07:07:41 +0000

    Catch creating channel error

--- dnachat/transmission.py
+++ dnachat/transmission.py
@@ -1,5 +1,6 @@
 # -*-coding:utf8-*-
 import bson
+import time
 from threading import Thread
 
 from .logger import logger
@@ -16,18 +17,27 @@ class Transmitter(Thread):
         pubsub.listen().next()
         for message in pubsub.listen():
             if message['channel'] == 'create_channel':
-                data = bson.loads(message['data'])
-                clients = reduce(lambda x, y: x.union(y), self.factory.channels.values(), set())
-                listening_users = dict((c.user.id, c) for c in clients if c.user)
-                channel_users = set(data['users'])
-                applying_users = set(listening_users.keys()) & channel_users
-                if applying_users:
-                    self.factory.channels.setdefault(data['channel']).extend(
-                        [listening_users[user] for user in applying_users]
-                    )
+                try:
+                    self.make_client_attend_channel(message)
+                except Exception, e:
+                    logger.error('CreatingChannelError: %s' % str(e), exc_info=True)
                 continue
+
             for client in self.factory.channels.get(message['channel'], []):
                 try:
+                    join_info = client.user.join_infos_dict[message['channel']]
+                    join_info.last_sent_at = time.time()
                     client.transport.write(message['data'])
                 except Exception, e:
                     logger.error('TransmissionError: %s' % str(e), exc_info=True)
+
+    def make_client_attend_channel(self, message):
+        data = bson.loads(message['data'])
+        clients = reduce(lambda x, y: x.union(y), self.factory.channels.values(), set())
+        listening_users = dict((c.user.id, c) for c in clients if c.user)
+        channel_users = set(data['users'])
+        applying_users = set(listening_users.keys()) & channel_users
+        if applying_users:
+            self.factory.channels.setdefault(data['channel']).extend(
+                [listening_users[user] for user in applying_users]
+            )
