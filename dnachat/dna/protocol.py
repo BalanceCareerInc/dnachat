68abc75b8daa8cf4722473a10b5c574730a9b0eb
Acuros <acuros@kaist.ac.kr>
2015-08-08 13:47:14 +0900

    Log packet and user id on error

--- dnachat/dna/protocol.py
+++ dnachat/dna/protocol.py
@@ -8,7 +8,8 @@ from ..logger import logger
 
 
 class DnaProtocol(Protocol):
-    def dataReceived(self, packet):
+    def dataReceived(self, raw_packet):
+        packet = raw_packet
         while True:
             try:
                 size = struct.unpack('<i', packet[:4])[0]
@@ -21,7 +22,11 @@ class DnaProtocol(Protocol):
                 logger.info('"%s" Received' % request.method)
                 self.requestReceived(request)
             except ProtocolError, e:
-                logger.error('ProtocolError: %s' % str(e), exc_info=True)
+                user_info = ''
+                if self.user:
+                    user_info = '({0}) - '.format(self.user.id)
+                logger.error('ProtocolError: {0}, {1}0x{2}'.format(str(e), user_info, raw_packet.encode('hex')),
+                             exc_info=True)
                 self.transport.loseConnection()
             if packet == '':
                 break
